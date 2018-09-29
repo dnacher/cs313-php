ed02efeb6f2886425d2a5671cf724d5bab8519ed
DesDanielNacher <daniel.nacher@despegar.com>
2018-09-29 15:06:16 -0300

    adding bootstrap for week assignment 2.17v

--- web/introduction.php
+++ web/introduction.php
@@ -26,7 +26,7 @@
   				<div class="introduction">
   					<p>Hello my name is Daniel, This is my page to introduce myself.
   					<br>I'm from Uruguay a little country between Argentina and Brazil<br>
-  					in my free time I like to solve Rubik's cube and play guitar.
+  					in my free time I like to solve Rubik's cube and play guitar.<br>
   					I'm working as a Java developer in a .com company.</p>
   				</div>
   			</div>

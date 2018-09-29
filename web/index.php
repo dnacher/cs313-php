fcf508f9b5e9b762b5a7637663fb7dda506545a5
DesDanielNacher <daniel.nacher@despegar.com>
2018-09-29 13:14:58 -0300

    add some information to index.php for week assignment

--- web/index.php
+++ web/index.php
@@ -1,5 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Index</title>
+		<link rel="stylesheet" href="/css/style.css">
+		<script src="/js/js.js"></script>
 
-  phpinfo();
+		<!-- BEGIN bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<!-- END   bootstrap -->
 
-?>
+	</head>
+	<body>
+		<?php echo '<h1>This is my main page from php!</h1>'; ?>
+
+	</body>
+</html>

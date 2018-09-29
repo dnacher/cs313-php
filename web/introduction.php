95361d20471fe5068f8232aa6ee2897bfcdac51a
DesDanielNacher <daniel.nacher@despegar.com>
2018-09-29 14:38:52 -0300

    adding bootstrap for week assignment 2.10v

--- web/introduction.php
+++ web/introduction.php
@@ -14,6 +14,7 @@
 		
 	</head>
 	<body class="body">
+		<?php echo '<h1>This is my main page from php!</h1>'; ?>
 		<div class="row no-gutters">
   			<div class="col-12 col-sm-6 col-md-8">
   				<form action="index.php">
@@ -25,7 +26,7 @@
   			</div>
 		</div>
 
-		<?php echo '<h1>This is my main page from php!</h1>'; ?>
+		
 
 	</body>
 </html>

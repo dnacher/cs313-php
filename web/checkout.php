8b71c514667ffd912ec963d8ba1e78de04be871b
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-09 00:05:55 -0300

    shopping cart assignment v1.11

--- web/checkout.php
+++ web/checkout.php
@@ -22,10 +22,11 @@
 
 <?php
   if (count($_SESSION['myproducts']) != 0) {
-    echo "<div class=\"checkout\"><h1>Checkout:</h1><form action=\"confirmed.php\" method=\"post\">
-        Street: <input required type=\"text\" name=\"street\"> <br>
-        State: <input type=\"text\" name=\"state\"> <br>
-        City: <input type=\"text\" name=\"city\"> <br>
+    echo "<div class=\"checkout\"><h1>Checkout:</h1><form action=\"purchase.php\" method=\"post\">
+        <div class=\"container\">
+        <div class=\"row\">Street: <input required type=\"text\" name=\"street\"></div> <br>
+        <div class=\"row\">State: <input type=\"text\" name=\"state\"></div> <br>
+        <div class=\"row\">City: <input type=\"text\" name=\"city\"></div> <br>
         <input type=\"submit\" value=\"Confirm Order\">
     </form></div>";
   }

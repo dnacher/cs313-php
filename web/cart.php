1da2a15769c1ed07c4791d2f9ac9fa30cd602aab
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-08 23:33:26 -0300

    shopping cart assignment v1.7

---
+++ web/cart.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    include('header.php');
+?>
+
+<h1>Cart Information</h1>
+
+<?php
+  if (count($_SESSION['myproducts']) == 0) {
+      echo "<p>Your cart is empty</p>";
+  }  
+  else {
+      echo "<p>You have:</p>";
+      foreach ($_SESSION['myproducts'] as $value) {
+          echo "<div class=\"product\">
+                <h3>$value</h3>
+                </div>";
+      }
+  }
+?>
+
+<a href="browse.php">Browse</a>
+<a href="checkout.php">Checkout</a>
+
+<?php
+   include('footer.php')
+?>

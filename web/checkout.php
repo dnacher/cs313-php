b3d25e07cf37e599d810e1c397e69b00e598dc55
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-08 23:56:49 -0300

    shopping cart assignment v1.10

--- web/checkout.php
+++ web/checkout.php
@@ -1,9 +1,13 @@
 <?php
+    // Start the session
     session_start();
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
     include('header.php');
 ?>
 
-<h2>Are you sure you want these items?</h2>
+<h2>Confirm that you want to checkout these items?</h2>
 
 <?php
   if (count($_SESSION['myproducts']) == 0) {
@@ -12,7 +16,7 @@
   else {
       echo "<p>You have:</p><ul>";
       foreach ($_SESSION['myproducts'] as $value) {
-          echo "<div class="alert alert-primary" role="alert"><li>$value</li></div>";
+          echo "<li>$value</li>";
       }
       echo "</ul>";
   }
@@ -20,12 +24,13 @@
 
 <?php
   if (count($_SESSION['myproducts']) != 0) {
-    echo "<p><div class=\"alert alert-success\" role=\"alert\">Your Cart is Empty</div></p>";
+    echo "<p>Your Cart is Empty</p>";
+    // With more time, validation would be a nice feature
     echo "<div class=\"checkout\"><h1>Checkout:</h1><form action=\"confirmed.php\" method=\"post\">
-        Street: <input required type=\"text\" name=\"street\"> <br>               
+        Street: <input required type=\"text\" name=\"street\"> <br>
         State: <input type=\"text\" name=\"state\"> <br>
         City: <input type=\"text\" name=\"city\"> <br>
-        <input type=\"submit\" value=\"Confirm\">
+        <input type=\"submit\" value=\"Confirm Order\">
     </form></div>";
   }
   else {

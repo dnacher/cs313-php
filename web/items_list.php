828fd14779618c4812cf05bb3d81a9f5d2a16688
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-08 23:05:13 -0300

    shopping cart assignment v1.1

---
+++ web/items_list.php
@@ -0,0 +1,49 @@
+<?php
+	include('header.php');
+	session_start();
+	$productCount = 5;
+	$prices = array(14.50, 8.50, 7.00, 23.70, 50.00);
+	$products = array("Rubik Cube 3x3x3", "Rubik 2x2x2", "Axis Cube", "Megaminx", "Rubik cube 9x9x9");
+
+?>
+	<body class="body">
+		<?php echo '<h1>Items</h1>'; ?>
+		<div class="alert alert-primary" role="alert">
+		  	Shopping Cart
+		  	<!-- Information -->
+			<h6>You have <?php echo count($_SESSION['myproducts']); ?> item(s) in your cart</h6>
+		</div>
+
+		<!-- Cart button -->
+		<form action="cart.php">
+			<button type="submit" class="btn btn-primary">Go to Cart</button><br><br>    		
+		</form>
+		<!-- checkout button -->
+		<form action="checkout.php">
+			<button type="submit" class="btn btn-primary">Checkout</button><br><br>    		
+		</form><br><br><br>
+		<!-- search button -->
+		<form action="browse.php" method="get">
+    		Search: <input type="text" name="search" value="<?php echo $_GET["search"] ?>">
+    		<input type="submit" value="Search">
+		</form>
+
+		<!-- fill the table with products -->
+		<?php
+  			for ($x = 0; $x < $productCount; $x++) {
+    			$found = strpos(strtolower($products[$x]), strtolower($_GET["search"]));
+    			if (!isset($_GET["search"]) || empty($_GET["search"]) || $found !== false ) {
+        			echo '<div class="product">
+			        <h2>' . ucfirst($products[$x]) . '</h2>
+			        <h3> $' . number_format($prices[$x], 2) . '</h3>
+			        <form action="addToCart.php" method="get">
+			        	<input type="hidden" name="product" value="' . $products[$x] . '"></input>
+			        	<input type="submit" value="add to cart"></input>
+        			</form>
+        			</div>';
+    			}
+  			}
+		?>
+
+
+<?php include('footer.php') ?>
--- web/shopping_cart.php
+++ web/shopping_cart.php
@@ -2,29 +2,17 @@
 	include('header.php');
 ?>
 	<body class="body">
-		<?php echo '<h1>This is my main page from php!</h1>'; ?>
+		<?php echo '<h1>Shopping Cart</h1>'; ?>
 		<div class="alert alert-primary" role="alert">
 		  Shopping Cart
 		</div>
 
 	<div class="button-center">
-		<form action="introduction.php">
-			<button type="submit" class="btn btn-primary">Introduction</button><br><br>    		
+		
+		<form action="items_list.php">
+			<button type="submit" class="btn btn-primary">Go to Items List</button><br><br>    		
 		</form>
-
-		<form action="projects.php">
-			<button type="submit" class="btn btn-primary">Projects for cs313</button><br><br>    	
-		</form>
-
-		<form action="form.php">
-			<button type="submit" class="btn btn-primary">Form Page</button><br><br> 
-		</form>
-
-
-		<form action="team_week_3.php">
-			<button type="submit" class="btn btn-primary">Team 7 Week 3</button><br><br> 
-		</form>
-
 
 	</div>
+
 <?php include('footer.php') ?>

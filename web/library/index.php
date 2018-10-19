79f33a349bb9325daba7535c1d385b66f70ca520
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-18 23:44:30 -0300

    add library project folder add form from h3school v1.5

--- web/library/index.php
+++ web/library/index.php
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Index</title>
     <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/js.js"></script>
 </head>
 <body>
 

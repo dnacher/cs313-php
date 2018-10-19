ca3916984916e78b64c76f8347513e6c09b0ea92
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-18 23:20:13 -0300

    add library project folder v1.3

--- web/library/index.php
+++ web/library/index.php
@@ -11,18 +11,16 @@
 		<script src="/js/js.js"></script>		
 	</head>
 
-	<body class="body">
-		<div class="wrapper">
-    		<form action="/library">
-    			<div class="form-group">
-	    			<label for="txtUser">User</label>
-	    			<input type="text" class="form-control mb-2" id="txtUser" placeholder="User name"><br>
-	    			<label for="txtPass">Password</label>
-	    			<input type="password" class="form-control mb-2" id="txtPass" placeholder="Password"><br>
-	    			<button type="submit" class="btn btn-primary">Login</button><br><br> 		
-				</div>
-			</form>
-		</div>	
+	<body class="body">		
+    	<form action="/library">
+    		<div class="form-group">
+	    		<label for="txtUser">User</label>
+	    		<input type="text" class="form-control mb-2" id="txtUser" placeholder="User name"><br>
+	    		<label for="txtPass">Password</label>
+	    		<input type="password" class="form-control mb-2" id="txtPass" placeholder="Password"><br>
+	    		<button type="submit" class="btn btn-primary">Login</button><br><br> 		
+			</div>
+		</form>			
 	</body>
 		
 </html>

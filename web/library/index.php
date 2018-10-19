0a672cbbdf5a74b2a871007646433ce2cf11d139
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-18 23:28:24 -0300

    add library project folder add form from h3school v1.5

--- web/library/index.php
+++ web/library/index.php
@@ -12,15 +12,31 @@
 	</head>
 
 	<body class="body">		
-    	<form action="/library">
-    		<div class="form-group">
-	    		<label for="txtUser">User</label>
-	    		<input type="text" class="form-control mb-2 mr-sm-2" id="txtUser" placeholder="User name"><br>
-	    		<label for="txtPass">Password</label>
-	    		<input type="password" class="form-control mb-2 mr-sm-2" id="txtPass" placeholder="Password"><br>
-	    		<button type="submit" class="btn btn-primary">Login</button><br><br> 		
+
+		<h2>Login</h2>
+
+		<form action="/action_page.php">
+  			<div class="imgcontainer">
+    			<img src="img_avatar2.png" alt="Avatar" class="avatar">
+  			</div>
+
+			<div class="container">
+		    	<label for="uname"><b>Username</b></label>
+		    	<input type="text" placeholder="Enter Username" name="uname" required>
+
+		    	<label for="psw"><b>Password</b></label>
+		    	<input type="password" placeholder="Enter Password" name="psw" required>
+		        
+		    	<button type="submit">Login</button>
+		    	<label>
+		      		<input type="checkbox" checked="checked" name="remember"> Remember me
+		    	</label>
+		  	</div>
+
+  			<div class="container" style="background-color:#f1f1f1">
+			    <button type="button" class="cancelbtn">Cancel</button>
+			    <span class="psw">Forgot <a href="#">password?</a></span>
 			</div>
-		</form>			
+		</form>
 	</body>
-		
 </html>

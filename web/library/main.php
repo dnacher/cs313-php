0f222017c6c4507d09b91bcc7a1f5cb9a0e819cb
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-21 00:00:46 -0300

    Prove : Assignment - PHP Data Access v1.1

--- web/library/main.php
+++ web/library/main.php
@@ -45,12 +45,10 @@ $statement->execute();
 echo '<div class="list-group">';
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
-  echo '<p>' . $row['name'] . '</p>';
-  echo '*******************';
   echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">';
   //echo    '<div class="d-flex w-100 justify-content-between">';
   echo        '<h5 class="mb-1">' . $row['name'] . '</h5>';
-  /*echo        '<small>' . $row['item_id'] . '</small>';*/
+  echo        '<small>' . $row['item_id'] . '</small>';
   //echo    '</div>'
   /*echo  '<p class="mb-1">' . $row[description] . '</p>';
   echo  '<small>' . $row[item_type_id]. '</small>';*/

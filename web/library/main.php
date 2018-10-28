95e7fa07b80646e897268dab3692aecd067930da
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-27 21:49:55 -0300

    Assignment DBUpdate v1.0

--- web/library/main.php
+++ web/library/main.php
@@ -18,33 +18,25 @@
 </head>
 
 <body>
-<div>
+  <div>
 
-<h1>Books Available</h1>
+    <h1>Books Available</h1>
 
-<?php
+    <?php
 
-echo 'aqui';
-
-
-$statement = $db->prepare("SELECT item_id, item_type_id,name, description FROM item");
-//$statement->execute();
-//echo '<div class="list-group">';
-/*while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-  echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">';
-  /*d-flex w-100 justify-content-between*/
-  /*echo '<div class="d-flex w-100 justify-content-between">';
-  echo        '<h5 class="mb-1">' . $row['name'] . '</h5>';
-  echo        '<small>' . $row['item_id'] . '</small>';
-  echo  '</div>';
-  echo  '<p class="mb-1">' . $row[description] . '</p>';
-  echo  '<small>' . $row[item_type_id]. '</small>';
-  echo '</a>';
-}
-echo '</div>';*/
-
-?>
-
-
-</div>
+        $statement = $db->prepare("SELECT item_id, item_type_id,name, description FROM item");
+        $statement->execute();
+        echo '<div class="list-group">';
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+          echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">';
+          echo '<div class="d-flex w-100 justify-content-between">';
+          echo        '<h5 class="mb-1">' . $row['name'] . '</h5>';
+          echo        '<small>' . $row['item_id'] . '</small>';
+          echo  '</div>';
+          echo  '<p class="mb-1">' . $row[description] . '</p>';
+          echo  '<small>' . $row[item_type_id]. '</small>';
+          echo '</a>';
+        }
+        echo '</div>';
+    ?>
+  </div>

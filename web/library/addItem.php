b27a011754107d1ce7a7fdbc5ba1756bb4dfcba3
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-27 23:31:08 -0300

    Assignment DBUpdate v1.2

---
+++ web/library/addItem.php
@@ -0,0 +1,54 @@
+<?php
+
+$txtName = $_POST['txtName'];
+$txtDescription = $_POST['txtDescription'];
+$cmbItemType = $_POST['cmbItemType'];
+require("dbConnect.php");
+$db = get_db();
+try
+{
+	$statement = $db->prepare("select max (item_id) from item");
+	$statement->execute();
+	$id = row[item_id];
+	echo $id;
+
+
+
+
+	/*$query = 'INSERT INTO item(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+	$statement = $db->prepare($query);
+	// Now we bind the values to the placeholders. This does some nice things
+	// including sanitizing the input with regard to sql commands.
+	$statement->bindValue(':book', $book);
+	$statement->bindValue(':chapter', $chapter);
+	$statement->bindValue(':verse', $verse);
+	$statement->bindValue(':content', $content);
+	$statement->execute();
+	// get the new id
+	$scriptureId = $db->lastInsertId("scripture_id_seq");
+	// Now go through each topic id in the list from the user's checkboxes
+	foreach ($topicIds as $topicId)
+	{
+		echo "ScriptureId: $scriptureId, topicId: $topicId";
+		// Again, first prepare the statement
+		$statement = $db->prepare('INSERT INTO scripture_topic(scriptureId, topicId) VALUES(:scriptureId, :topicId)');
+		// Then, bind the values
+		$statement->bindValue(':scriptureId', $scriptureId);
+		$statement->bindValue(':topicId', $topicId);
+		$statement->execute();
+	}
+}
+catch (Exception $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error with DB. Details: $ex";
+	die();
+}
+// finally, redirect them to a new page to actually show the topics
+header("Location: showTopics.php");
+die(); // we always include a die after redirects. In this case, there would be no
+       // harm if the user got the rest of the page, because there is nothing else
+       // but in general, there could be things after here that we don't want them
+       // to see.*/
+?>

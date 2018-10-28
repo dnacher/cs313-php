881be5db6ec78de9b0b1a5c2518b69b0b92d8582
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-28 01:55:11 -0300

    Assignment DBUpdate v1.2

--- web/library/deleteItem.php
+++ web/library/deleteItem.php
@@ -7,14 +7,14 @@ $id = $_POST['id_item'];
 try{
 	$query = 'DELETE item WHERE item_id=' . $id;
 	echo $query;
-	/*
+	
 	$statement = $db->prepare($query);
-	$statement->bindValue(':id', $id);
-	$statement->execute();*/
+	//$statement->bindValue(':id', $id);
+	$statement->execute();
 }catch (Exception $ex){
 	echo "Error with DB. Details: $ex";
 	die();
 }
-//header("Location: main.php");
+header("Location: main.php");
 die(); 
 ?>

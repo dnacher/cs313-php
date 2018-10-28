cd6c2ac1d7db886574509f76da89a13d9599f419
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-27 21:33:50 -0300

    Assignment DBUpdate v1.0

--- web/library/main.php
+++ web/library/main.php
@@ -25,12 +25,12 @@
 
 $statement = $db->prepare("SELECT item_id, item_type_id,name, description FROM item");
 $statement->execute();
-echo '<div class="list-group">';
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+//echo '<div class="list-group">';
+/*while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
   echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">';
   /*d-flex w-100 justify-content-between*/
-  echo '<div class="d-flex w-100 justify-content-between">';
+  /*echo '<div class="d-flex w-100 justify-content-between">';
   echo        '<h5 class="mb-1">' . $row['name'] . '</h5>';
   echo        '<small>' . $row['item_id'] . '</small>';
   echo  '</div>';
@@ -38,7 +38,8 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   echo  '<small>' . $row[item_type_id]. '</small>';
   echo '</a>';
 }
-echo '</div>';
+echo '</div>';*/
+echo 'aqui';
 ?>
 
 

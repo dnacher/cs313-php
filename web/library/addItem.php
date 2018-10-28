980ce73ca893d91a3caac83a7b08113d63cf1e29
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-27 23:46:25 -0300

    Assignment DBUpdate v1.2

--- web/library/addItem.php
+++ web/library/addItem.php
@@ -9,7 +9,7 @@ $db = get_db();
 try{
 	$statement = $db->prepare("select max (item_id) from item");
 	$statement->execute();
-	$id = $row[max_item_id];
+	$id = $row;
 	echo "esto es: " . $id;
 }catch (Exception $ex){
 	echo "Error with DB. Details: $ex";

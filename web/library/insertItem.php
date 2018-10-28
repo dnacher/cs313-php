928ea35ddeb63fb6c7696e5c036f1cfd5592cbea
DesDanielNacher <daniel.nacher@despegar.com>
2018-10-28 00:30:26 -0300

    Assignment DBUpdate v1.2

--- web/library/insertItem.php
+++ web/library/insertItem.php
@@ -15,7 +15,7 @@
 
 <body>
   
-<form action="additem.php" method="post">
+<form action="addItem.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">

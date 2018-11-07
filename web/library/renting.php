7e1e9f2d32af3d89884822ddb7951fbdb061b48c
DesDanielNacher <daniel.nacher@despegar.com>
2018-11-07 00:28:12 -0300

    Project 01 v1.0

--- web/library/renting.php
+++ web/library/renting.php
@@ -25,13 +25,8 @@ $item_id = $_POST['item_id'];
 try{
 	$date1 = date("Y-m-d");
 	$date2 = date('Y-m-d', strtotime("+7 days"));
-	echo "d1 $date1 <br>";
-	echo "d2 $date2 <br>";
-	echo "uid $user_id <br>";
-	echo "iid $item_id <br>";
 	$query = "INSERT INTO rent_table (user_id, item_id, date_start, date_end)
-	 VALUES ('$user_id', '$item_id', date1, date2)";
-
+	 VALUES ('$user_id', '$item_id', $date1, $date2)";
 	$statement = $db->prepare($query);
 	$statement->execute();
 
@@ -39,6 +34,6 @@ try{
 	echo "Error with DB. Details: $ex";
 	die();
 }
-/*header("Location: main.php");
-die();*/
+header("Location: main.php");
+die();
 ?>

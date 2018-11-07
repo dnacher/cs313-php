9ee486d4943c0b3bac227261dd76d29e73381c8a
DesDanielNacher <daniel.nacher@despegar.com>
2018-11-07 00:14:42 -0300

    Project 01 v1.0

--- web/library/renting.php
+++ web/library/renting.php
@@ -1,6 +1,23 @@
 <?php
 require("dbconnect.php");
 $db = get_db();
+session_start();
+  $txtUser = $_SESSION['user'];
+  $txtUserType = $_SESSION['userType'];
+  $user_id = $_SESSION['user_id'];
+  if(isset($txtUser)){
+  	if(txtUserType!=3){
+  		//this page is only for students.
+  		header("Location: index.php");
+    	die();	
+  	}
+    echo $txtUser . "<br>";
+    echo $txtUserType . "<br>";
+    echo 'correct';    
+  }else{
+    header("Location: index.php");
+    die();
+  }    
 
 $user_id = $_POST['user_id'];
 $item_id = $_POST['item_id'];
